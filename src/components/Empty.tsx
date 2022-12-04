df52f0ad707960bf4cdd6fe6bd32ae9a137b30d0
Luis Rodriguez <luisrodge@outlook.com>
2022-12-04 17:00:41 -0600

    Add empty component

---
+++ src/components/Empty.tsx
@@ -0,0 +1,7 @@
+export default function Empty() {
+  return (
+    <div className="flex-1 text-center">
+      <h1 className="tex-xl text-slate-800">No listings to show</h1>
+    </div>
+  );
+}
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
+import Empty from "../../components/Empty";
 import ListingPreview from "../../components/ListingPreview";
 import Map from "../../components/Map";
 import { useCurrentLocation } from "../../hooks/useCurrenLocation";
@@ -16,38 +17,29 @@ const Search: NextPage = () => {
       { enabled: !!location }
     );
 
-  const { data: listings } = trpc.listings.getAll.useQuery();
-
   return (
-    <div className="">
+    <div>
       {nearByIsLoading && <p>Loading nearby businesses...</p>}
 
-      {nearByListings && (
-        <div className="flex ">
-          <div className="mt-32 flex flex-1">
+      <div className="flex ">
+        <div className="mt-32 flex flex-1">
+          {nearByListings && (
             <ul className="grid grid-cols-3 gap-8 py-4 px-8">
               {nearByListings.map((listing) => (
                 <ListingPreview listing={listing} key={listing.id} />
               ))}
             </ul>
-          </div>
-          {location && (
-            <div className="w-2/6">
-              <div className="fixed right-0 top-20 h-full w-2/6 border-l bg-white">
-                <Map coords={location} listings={nearByListings} />
-              </div>
-            </div>
           )}
+          {!nearByIsLoading && !nearByListings?.length && <Empty />}
         </div>
-      )}
-
-      {!nearByIsLoading && !nearByListings && listings && (
-        <ul className="grid grid-cols-3 gap-10">
-          {listings.map((listing) => (
-            <ListingPreview listing={listing} key={listing.id} />
-          ))}
-        </ul>
-      )}
+        {location && (
+          <div className="w-2/6">
+            <div className="fixed right-0 top-20 h-full w-2/6 border-l bg-white">
+              <Map coords={location} listings={nearByListings} />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
